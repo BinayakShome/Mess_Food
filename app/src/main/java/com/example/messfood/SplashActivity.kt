bb784f89a0a28ad0ddbe1cbf362410230c868de9
Binayak Shome <binayakshome3@gmail.com>
2025-01-21 19:13:14 +0530

    Fix: Minor changes

--- app/src/main/java/com/example/messfood/SplashActivity.kt
+++ app/src/main/java/com/example/messfood/SplashActivity.kt
@@ -25,6 +25,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3125)
+        }, 3050)
     }
 }
